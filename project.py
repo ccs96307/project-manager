b86f21496ee1a0a2fbd44aaa70baa679aa561503
ccs96307 <ccs96307@gmail.com>
2021-08-14 02:11:36 +0800

    raw_data

--- project.py
+++ project.py
@@ -3,22 +3,43 @@
 
 class Project(object):
     def __init__(self):
-        # Attributes
+        # Project
         self.project_name = ""
         self.project_is_clear = False
+
+        # Tasks
         self.tasks = []
         self.weights = []
         self.comments = []
-        self.is_clear = []
+        self.is_clear_list = []
 
-    def add_task(self, task: str):
-        if task == str:
-            self.tasks.append(task)
-        else:
-            # TODO
-            None
+    def add_task(self, task="", weight=1, comment="", clear=False):
+        # If task field is null
+        if task == "":
+            task = "temp_{}".format(len(self.tasks))
 
-    
+        # Add
+        self.tasks.append(task)
+        self.weights.append(weight)
+        self.comments.append(comment)
+        self.is_clear_list.append(clear)
+
+    def delete_task(self, index: int):
+        self.tasks.pop(index)
+        self.weights.pop(index)
+        self.comments.pop(index)
+        self.is_clear_list.pop(index)
+
+    def update_task(self, index, task, weight, comment, clear):
+        self.tasks[index] = task
+        self.weights[index] = weight
+        self.comments[index] = comment
+        self.is_clear_list[index] = clear
+
+
+
+
+
 
 
 
